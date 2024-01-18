5314fa82b42a82f9be41659d59c6f9dd740ac2ae
Jay-vaghani <jayvaghani10@gmail.com>
2024-01-18 15:09:06 +0530

    navbar edit

--- src/components/layout/BootstrapNavbar/MobileNav.jsx
+++ src/components/layout/BootstrapNavbar/MobileNav.jsx
@@ -9,9 +9,6 @@ import Accordion from "react-bootstrap/Accordion";
 import { useNavigate } from "react-router-dom";
 
 function MobileNavBar() {
-
-
-
   const width = {
     xs: "120px",
     sm: "170px",
@@ -20,19 +17,15 @@ function MobileNavBar() {
 
   let navigate = useNavigate();
 
-  const [menu, setMenu] = useState(null)
-
+  const [menu, setMenu] = useState(null);
 
   useEffect(() => {
-
-    setMenu(document.getElementsByClassName('navbar-collapse')[0])
-
-
-  })
+    setMenu(document.getElementsByClassName("navbar-collapse")[0]);
+  });
 
   const closeMenu = () => {
-    menu.classList.remove("show")
-  }
+    menu.classList.remove("show");
+  };
 
   return (
     <Navbar expand="xl">
@@ -49,181 +42,180 @@ function MobileNavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link onClick={() => navigate("/")
-            }>HOME</Nav.Link>
+            <Nav.Link onClick={() => navigate("/")}>HOME</Nav.Link>
             <NavDropdown title="ABOUT US" id="basic-nav-dropdown">
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/about/skect")
-              }
-              }>
-                Sponsoring Body-SKECT</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/about/our-leadership")
-              }
-              }>
-                OUR LEADERSHIP</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/about/kpgu-vadodara")
-              }
-              }>
-                KPGU Vadodara</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/about/achievements")
-              }
-              }>
-                SIGNIFICANT ACHIEVEMENTS</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/about/vision-mission")
-              }
-              }>
-                VISION & MISSION</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/about/dr-kiran-c-patel-message")
-              }
-              }>
-                MESSAGE FROM DR. KIRAN C. PATEL</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/about/message-from-the-presidents-desk")
-              }
-              }>
-                MESSAGE FROM THE PRESIDENT'S DESK</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/about/message-from-the-provost")
-              }
-              }>
-                MESSAGE FROM THE PROVOST</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/about/governance/governing-body")
-              }
-              }>
-                GOVERNANCE</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/about/university-officers")
-              }
-              }>
-                UNIVERSITY OFFICERS</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/about/statutory-recognition")
-              }
-              }>
-                STATUTORY RECOGNITION</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/about/approvals/institute-of-technology")
-              }
-              }>
-                APPROVALS</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/about/awards-accolades")
-              }
-              }>
-                AWARDS & ACCOLADES</NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/about/skect");
+                }}
+              >
+                Sponsoring Body-SKECT
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/about/our-leadership");
+                }}
+              >
+                OUR LEADERSHIP
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/about/kpgu-vadodara");
+                }}
+              >
+                KPGU Vadodara
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/about/achievements");
+                }}
+              >
+                SIGNIFICANT ACHIEVEMENTS
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/about/vision-mission");
+                }}
+              >
+                VISION & MISSION
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/about/dr-kiran-c-patel-message");
+                }}
+              >
+                MESSAGE FROM DR. KIRAN C. PATEL
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/about/message-from-the-presidents-desk");
+                }}
+              >
+                MESSAGE FROM THE PRESIDENT'S DESK
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/about/message-from-the-provost");
+                }}
+              >
+                MESSAGE FROM THE PROVOST
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/about/governance/governing-body");
+                }}
+              >
+                GOVERNANCE
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/about/university-officers");
+                }}
+              >
+                UNIVERSITY OFFICERS
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/about/statutory-recognition");
+                }}
+              >
+                STATUTORY RECOGNITION
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/about/approvals/institute-of-technology");
+                }}
+              >
+                APPROVALS
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/about/awards-accolades");
+                }}
+              >
+                AWARDS & ACCOLADES
+              </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="ACADEMIC" id="basic-nav-dropdown">
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("academics/faculty-of-engineering-and-technology")
-              }
-              }>
-                Faculty of Engineering Technology</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("academics/faculty-of-medicine-and-healthcare/krishna-school-of-pharmacy-and-research/ug")
-              }
-              }>
-                Faculty Of Medicine & Healthcare</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("academics/faculty-of-sciences/krishna-school-of-sciences/ug")
-              }
-              }>
-                Faculty Of Science</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("academics/faculty-of-commerce-and-management/krishna-school-of-business-management/ug")
-              }
-              }>
-                Faculty Of Commerce & Management</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("academics/faculty-of-humanities-and-policy-studies/krishna-school-of-arts-and-humanities/ug")
-              }
-              }>
-                Studies Policy & Humanities OfFaculty</NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("academics/faculty-of-engineering-and-technology");
+                }}
+              >
+                Faculty of Engineering Technology
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate(
+                    "academics/faculty-of-medicine-and-healthcare/krishna-school-of-pharmacy-and-research/ug"
+                  );
+                }}
+              >
+                Faculty Of Medicine & Healthcare
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate(
+                    "academics/faculty-of-sciences/krishna-school-of-sciences/ug"
+                  );
+                }}
+              >
+                Faculty Of Science
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate(
+                    "academics/faculty-of-commerce-and-management/krishna-school-of-business-management/ug"
+                  );
+                }}
+              >
+                Faculty Of Commerce & Management
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate(
+                    "academics/faculty-of-humanities-and-policy-studies/krishna-school-of-arts-and-humanities/ug"
+                  );
+                }}
+              >
+                Studies Policy & Humanities OfFaculty
+              </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="CAMPUS" id="basic-nav-dropdown">
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/campus/infrastructure/smart-classrooms")
-              }
-              }>
-                Infrastructure</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/campus/gallery")
-              }
-              }>
-                Gallery</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/campus/")
-              }
-              }>
-                ATM & Bank Facilities</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/campus/")
-              }
-              }>
-                Dining & Cafeteria</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/campus/")
-              }
-              }>
-                Health Facilities</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/campus/")
-              }
-              }>
-                Library & Information System</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/campus/")
-              }
-              }>
-                Safety & Security</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/campus/")
-              }
-              }>
-                Environment</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/campus/")
-              }
-              }>
-                Arts & Cultural Programs</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {
-                closeMenu()
-                navigate("/campus/")
-              }
-              }>
-                Sports & Recreation</NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("campus/campus-life");
+                }}
+              >
+                Infrastructure
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  closeMenu();
+                  navigate("/campus/gallery");
+                }}
+              >
+                Gallery
+              </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="ADMISSIONS" id="basic-nav-dropdown">
               <NavDropdown.Item>UG PROGRAMMES</NavDropdown.Item>
@@ -237,7 +229,10 @@ function MobileNavBar() {
               <NavDropdown.Item>EXAMINATION SYSTEM</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="#link">PLACEMENTS</Nav.Link>
-            <NavDropdown title="RESEARCH & PUBLICATIONS" id="basic-nav-dropdown">
+            <NavDropdown
+              title="RESEARCH & PUBLICATIONS"
+              id="basic-nav-dropdown"
+            >
               <NavDropdown.Item>RESEARCH & INNOVATION</NavDropdown.Item>
               <NavDropdown.Item>START-UPS</NavDropdown.Item>
               <NavDropdown.Item>PUBLICATIONS</NavDropdown.Item>
